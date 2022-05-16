8013897a8f5ec1b77d3f4a6bcebda5e85ecbf0f8
Alexander Winkler <awinkler@bgc-jena.mpg.de>
2022-05-16 18:12:33 +0200

    Add CMIP6 data and the respective processing script; restructure directories

---
+++ simple_gpp_model/process_predictor-variables_CMIP6.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 
+Title: Simple script to process EAR5 and AHVRR Fpar to times series
+@author: awinkler
+"""
+
+## import modules
+import glob
+import xarray as xr
+import pandas as pd
+import numpy as np
+
+## some util functions
+#%% Calculate saturation vapor pressure
+def vapor_saturated(t2m):
+    # L is the latent heat of vaporization, 2.5×10^6 J kg−1
+    # Rv is the gas constant for water vapor 461 J K−1 kg−1
+    # t2m is 2m air temp in Celsius
+
+    L = 2.5e6
+    Rv = 461
+    es = 6.11 * np.exp((L / Rv) * (1 / 273 - 1 / (273 + t2m)))
+
+    return es # in hPa
+
+#%% Calculate Vapor pressure deficit
+def vapor_pressure_deficit(t2m, rh):
+    # t2m is 2m air temp in Celsius
+    # rh is relative humidity in %
+
+    ## calculate saturation vapor pressure
+    es = vapor_saturated(t2m)
+
+    ## calculate vapor pressure deficit
+    vpd = ((100 - rh) / 100) * es
+
+    return vpd  # in hPa 
+
+#%% Calculate fAPAR from LAI
+def fapar_from_lai(lai, k=0.54):
+    # lai is leaf area index
+    # k is the light extinction coefficient; 
+    # 0.5 for conifers, 0.58 for broadleaf (Jung et al., BG, 2007)
+    return 1 - np.exp(-k * lai)
+
+#%% obtain time-series for specific variables 
+def obtain_ts(path, var, domain): 
+    ds = xr.open_mfdataset(glob.glob(path+'/'+domain+'/'+var+'/**/*.nc', recursive=True)) 
+    ts = ds.sel(lat=lat, lon=lon, method='nearest')[var].to_dataframe() 
+    return ts
+
+## Jena (Germany) coordinates
+lon = 11.5892
+lat = 50.9271
+
+## on DKRZ
+MIP = "CMIP" # ScenarioMIP
+simulation = "historical" # ssp585 
+CMIP6 = "/pool/data/CMIP6/data/"+MIP+"/MPI-M/MPI-ESM1-2-LR/"+simulation+"/r1i1p1f1/"
+
+rsds = obtain_ts(CMIP6, 'rsds', 'day') # Surface Downwelling Shortwave Radiation
+tasmin = obtain_ts(CMIP6, 'tasmin', 'day')
+mrso = obtain_ts(CMIP6, 'mrso', 'day') # Total Soil Moisture Content
+mrsos = obtain_ts(CMIP6, 'mrsos', 'day') # Upper Soil Moisture Content
+tas = obtain_ts(CMIP6, 'tas', 'day')
+hurs = obtain_ts(CMIP6, 'hurs', 'day') # Near-Surface Relative Humidity
+lai = obtain_ts(CMIP6, 'lai', 'Eday') # Leaf area index
+
+# harmonize units
+tas['tas'] = tas['tas'] - 273.15 # K to Celsius
+tasmin['tasmin'] = tasmin['tasmin'] - 273.15 # K to Celsius
+
+# calculate VPD
+vpd = vapor_pressure_deficit(tas['tas'], hurs['hurs']) * 100 # hPa to Pa
+
+# calculate FAPAR
+fapar = fapar_from_lai(lai['lai'])
+
+# put all together
+predictors = pd.concat([rsds, mrso['mrso'], mrsos['mrsos'], tasmin['tasmin'], lai['lai']], axis=1)
+predictors['vpd'] = vpd
+predictors['fapar'] = fapar
+
+# store to disk
+predictors.to_csv('data/predictor-variables_Jena_MPI-ESM1-2-LR_'+simulation+'.csv')
+
