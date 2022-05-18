d2a8d914ab6912b60c4936caa91dd3cd3b6cd2bd
CR <creimers@bgc-jena.mpg.de>
2022-05-18 11:18:51 +0200

    made fPAR to fpar and some other changes

--- pytorch_utils/hybrid_model.py
+++ pytorch_utils/hybrid_model.py
@@ -10,7 +10,19 @@ class HybridModel(nn.Module):
             replace_tmin = False,
             replace_apar = False,
             replace_swc = False,
-            replace_combine = False):
+            replace_combine = False
+            mean_vpd = 0,
+            mean_tmin = 0,
+            mean_swrad = 0,
+            mean_fpar = 0,
+            mean_swc = 0,
+            std_vpd = 1,
+            std_tmin = 1,
+            std_swrad = 1,
+            std_fpar = 1,
+            std_swc = 1,
+            data_frame = None,
+            ):
 
         super(HybridModel, self).__init__()
         self.replace_vpd = replace_vpd
@@ -29,15 +41,33 @@ class HybridModel(nn.Module):
         self.fca = nn.linear(128,1)
         self.relu = nn.ReLU()
 
+        if not data_frame is None:
+            self.get_mean_std()
+        else: 
+            self.mean_vpd = mean_vpd
+            self.mean_tmin = mean_tmin
+            self.mean_swrad = mean_swrad
+            self.mean_fpar = mean_fpar
+            self.mean_swc = mean_swc
+            self.std_vpd = std_vpd
+            self.std_tmin = std_tmin
+            self.std_swrad = std_swrad
+            self.std_fpar = std_fpar
+            self.std_swc = std_swc
+
+
+
+
     def forward(self,x):
 
         swrad = x[:,0:1]
         tmin = x[:, 1:2]
         vpd = x[:, 2:3]
-        fPAR = x[:, 3:4]
+        fpar = x[:, 3:4]
         swc = x[:, 4:5]
        
         if self.replace_vpd:
+            vpd = (vpd - self.mean_vpd) / self.std_vpd
             vpd_scalar_0 = self.fc1(vpd)
             vpd_scalar_0 = self.relu(vpd_scalar_0)
             vpd_scalar = self.fc2(vpd_scalar_0)
@@ -45,26 +75,30 @@ class HybridModel(nn.Module):
             vpd_scalar = 1 - (torch.clamp(vpd, 650, 2400) - 650) / 1750
 
         if self.replace_swc:
+            swc = (swc - self.mean_swc) / self.std_swc
             swc_scalar = self.fc9(swc)
             swc_scalar = self.relu(swc_scalar)
             swc_scalar = self.fca(swc)
         else:
-            swc = ((swc - 25) / (100 - 25))**0.383
+            swc = ((swc - 0.1) / (0.25 - 0.1))**0.383
             swc = torch.clamp(swc, 0, 1)
 
         if self.replace_tmin:
+            tmin = (tmin - self.mean_tmin) / self.std_tmin
             tmin_scalar = self.fc3(tmin)
             tmin_scalar = self.relu(tmin_scalar)
-            tmin_scalar = self.fc4(tmin)_scalar)
+            tmin_scalar = self.fc4(tmin_scalar)
         else:
             tmin_scalar = (torch.clamp(tmin, -7, 9.5) + 7) / 16.5
         
         if self.replace_apar:
-            apar = self.fc7(torch.concat([swrad, fPAR], axis = -1))
+            swrad = (swrad - self.mean_swrad) / self.std_swrad
+            fpar = (fpar - self.mean_fpar) / self.std_fpar
+            apar = self.fc7(torch.concat([swrad, fpar], axis = -1))
             apar = self.relu(apar)
             apar = self.fc8(apar)
         else:
-            apar = (swrad * 0.45) * fPAR
+            apar = (swrad * 0.45) * fpar
 
         if self.replace_combine:
             out = self.fc5(torch.concat([tmin_scalar, vpd_scalar, apar, swc_scalar], axis = -1))
