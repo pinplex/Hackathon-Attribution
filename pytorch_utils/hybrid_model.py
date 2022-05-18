d1c2ccdc82aed749a148786b9b9133602b21f71e
CR <creimers@bgc-jena.mpg.de>
2022-05-18 10:50:43 +0200

    added the swc to the hybrid model

--- pytorch_utils/hybrid_model.py
+++ pytorch_utils/hybrid_model.py
@@ -9,6 +9,7 @@ class HybridModel(nn.Module):
             replace_vpd = True,
             replace_tmin = False,
             replace_apar = False,
+            replace_swc = False,
             replace_combine = False):
 
         super(HybridModel, self).__init__()
@@ -20,10 +21,12 @@ class HybridModel(nn.Module):
         self.fc2 = nn.Linear(128, 1)
         self.fc3 = nn.Linear(1, 128)
         self.fc4 = nn.Linear(128, 1)
-        self.fc5 = nn.Linear(3, 128)
+        self.fc5 = nn.Linear(4, 128)
         self.fc6 = nn.Linear(128, 1)
         self.fc7 = nn.Linear(2, 128)
         self.fc8 = nn.Linear(128, 1)
+        self.fc9 = nn.Linear(1, 128)
+        self.fca = nn.linear(128,1)
         self.relu = nn.ReLU()
 
     def forward(self,x):
@@ -32,14 +35,22 @@ class HybridModel(nn.Module):
         tmin = x[:, 1:2]
         vpd = x[:, 2:3]
         fPAR = x[:, 3:4]
+        swc = x[:, 4:5]
        
-
         if self.replace_vpd:
             vpd_scalar_0 = self.fc1(vpd)
             vpd_scalar_0 = self.relu(vpd_scalar_0)
             vpd_scalar = self.fc2(vpd_scalar_0)
         else:
             vpd_scalar = 1 - (torch.clamp(vpd, 650, 2400) - 650) / 1750
+
+        if self.replace_swc:
+            swc_scalar = self.fc9(swc)
+            swc_scalar = self.relu(swc_scalar)
+            swc_scalar = self.fca(swc)
+        else:
+            swc = ((swc - 25) / (100 - 25))**0.383
+            swc = torch.clamp(swc, 0, 1)
 
         if self.replace_tmin:
             tmin_scalar = self.fc3(tmin)
@@ -56,10 +67,10 @@ class HybridModel(nn.Module):
             apar = (swrad * 0.45) * fPAR
 
         if self.replace_combine:
-            out = self.fc5(torch.concat([tmin_scalar, vpd_scalar, apar], axis = -1))
+            out = self.fc5(torch.concat([tmin_scalar, vpd_scalar, apar, swc_scalar], axis = -1))
             out = self.relu(out)
             out = self.fc6(out)
         else:
             out =  tmin_scalar * vpd_scalar * apar 
 
-        return lue * out
+        return self.lue * out
