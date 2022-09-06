efc02e02114794fd6880088e37fe7b45f27fa333
Basil Kraft <bkraft@bgc-jena.mog.de>
2022-09-06 11:15:29 +0200

    rename file

---
+++ benchmark.py
@@ -0,0 +1,19 @@
+
+import os
+import shutil
+
+from hackathon.models.linear import LinearRunner
+
+models = [LinearRunner]
+
+for Runner in models:
+    model_name = Runner.__module__.split('.')[-1]
+    log_dir = f'./hackathon/logs/{model_name}'
+    if os.path.isdir(log_dir):
+        shutil.rmtree(log_dir)
+
+    # Training.
+    runner = Runner(log_dir=log_dir, seed=910)
+    trainer, datamodule, model = runner.train()
+    runner.predict(trainer=trainer, datamodule=datamodule, version='final')
+    runner.save_model(model=model, version='final')
