a7ccf342ad22b34fe07860727d8061c8d57962b9
Basil Kraft <29065166+bask0@users.noreply.github.com>
2022-11-21 17:15:10 +0100

    rework structure and documentation

--- hackathon/base_explanation.py
+++ hackathon/base_explanation.py
@@ -8,24 +8,32 @@ from hackathon.base_model import BaseModel
 from hackathon.data_pipeline import TSData
 
 class Explainer(object):
-    """Base explanation class, meant to be subclassed."""
+    """Base explanation class, meant to be subclassed.
+
+    Usage
+    -----
+    > Subclass this and at least override the `Explainer._custom_explanations` method.
+    > Explainer.get_explanations must exactly follow the signature defined here (see 'Parameters' 
+        and 'Returns'.)
+    > You may first calculate the full sensitivities and derive the other return items from that.
+        For sensitivities, we speak about sensitivity of a target *towards* a feature. We store the
+        sensitivity of the last four years of the test data towards all time steps of the features.
+        Make sure that you follow this patter; For each of the last four years days (= 1461 values),
+        calculate the sensitivity towards each time step. This yields a sensitivity tensor of
+        - 1461 x num_time x num_features for each batch element, or
+        - num_batch x 1461 x num_time x num_features in total.
+    > The sensitivitiy tensor can be assigned to an xr.Dataset by using the
+        `test_dataloader.dataset.assign_sensitivities(sensitivities=..., data_sel=batch['data_sel'])` method.
+    > Don't call `_custom_explanations`, use `get_explanations` instead.
+
+    """
 
     @abstractmethod
-    def get_explanations(
+    def _custom_explanations(
             self,
             model: BaseModel,
             test_dataloader: DataLoader) -> tuple[tuple[float], float, xr.Dataset]:
         """Returns explanations in a predefined format. Must be overridden.
-
-        Notes
-        -----
-        The sensitivities represent the last four years of the target data (length of 1461) towards
-        the entire time range. For each location, the dimensionality of the sensitivity tensor is
-        1461 x seq_len x num_features, where seq_len is the number of days in the dataset. E.g., for a dataset
-        of 8 years, the sensitivities have shape 365 * 4 + 1 = 1461 times 365 * 8 + 2 = 2922 times the number
-        of features -> 1461 x 2922 x 8. The tensor can be assigned to an xarray.Dataset using the DataLoader.dataset.assign_sensitivities method. The method expects a tensor of shape:
-            - (batch_size x 1461 x seq_len x num_features)
-        ...along with batch['data_sel'] to assign the sensitivities to the right location. 
 
         Parameters
         ----------
@@ -35,17 +43,16 @@ class Explainer(object):
         Returns
         -------
         Tuple of three elements:
-        - Dummy variable probability: a tuple containing the probability of each of the 8 input
-        features to be the dummy variable (which has no impact on the predictions).
+        - Dummy variable probability: a tuple containing the probability of each of the input
+          features to be the dummy variable (which has no impact on the predictions).
         - The GPP sensitivity towards CO2, a single float.
         - The sensitivities of GPP towards all variables, an xarray.Dataset. See 'Notes' for more details.
         """
 
-        model.eval()
         ds: TSData = test_dataloader.dataset
         for batch_idx, batch in enumerate(test_dataloader):
             # Do explanations.
-            s = np.random.normal(size=(batch['x'].shape[0], 1461, len(ds.ds.time), 8))
+            s = np.random.normal(size=(batch['x'].shape[0], 1461, len(ds.ds.time), ds.num_features))
             # Assign explanations.
             ds.assign_sensitivities(sensitivities=s, data_sel=batch['data_sel'])
 
@@ -59,3 +66,27 @@ class Explainer(object):
             co2_sens,
             ds.sensitivities,
         )
+
+    def get_explanations(
+            self,
+            model: BaseModel,
+            test_dataloader: DataLoader) -> tuple[tuple[float], float, xr.Dataset]:
+        """Returns explanations.
+
+        Parameters
+        ----------
+        model: a tuned model, subclass of BaseModel.
+        test_dataloader: a dataloader.
+
+        Returns
+        -------
+        Tuple of three elements:
+        - Dummy variable probability: a tuple containing the probability of each of the input
+          features to be the dummy variable (which has no impact on the predictions).
+        - The GPP sensitivity towards CO2, a single float.
+        - The sensitivities of GPP towards all variables, an xarray.Dataset. See 'Notes' for more details.
+
+        """
+
+        model.eval()
+        return self._custom_explanations(model=model, test_dataloader=test_dataloader)
