223ccc6083f84a7f25bbce942f98f50df1294fef
Basil Kraft <bkraft@bgc-jena.mog.de>
2022-11-24 11:41:55 +0100

    wip

--- benchmark.py
+++ benchmark.py
@@ -6,14 +6,22 @@ from argparse import ArgumentParser, Namespace
 from hackathon.model_runner import ModelRunner
 
 from hackathon.models.attn import model_setup as attn_model
-from hackathon.models.Conv1D import model_setup as conv1d
+from hackathon.models.Conv1D import model_setup as conv1d_model
 from hackathon.models.linear import model_setup as linear_model
-from hackathon.models.LSTM import model_setup as lstm
+from hackathon.models.LSTM import model_setup as lstm_model
 from hackathon.models.multimodel import model_setup as efficiency_model
-from hackathon.models.resnet import model_setup as resnet
-from hackathon.models.simplemlp import model_setup as simplemlp
+from hackathon.models.resnet import model_setup as resnet_model
+from hackathon.models.simplemlp import model_setup as simplemlp_model
 
-model_funs = [attn_model, conv1d, linear_model, lstm, efficiency_model, resnet, simplemlp]
+model_funs = [
+    attn_model,
+    conv1d_model,
+    linear_model,
+    lstm_model,
+    efficiency_model,
+    resnet_model,
+    simplemlp_model
+]
 
 def main(args: Namespace):
     for model_fn in model_funs:
