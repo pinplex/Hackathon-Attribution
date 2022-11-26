4c01ecabf3e87e60316c2873c81bbc27bbb21fea
Basil Kraft <29065166+bask0@users.noreply.github.com>
2022-11-26 17:37:37 +0100

    reworked explain loop

--- explain.py
+++ explain.py
@@ -1,6 +1,9 @@
 
 from argparse import ArgumentParser, Namespace
 import torch
+import os
+import shutil
+from tqdm import tqdm
 
 from hackathon.model_runner import ModelRunner
 from hackathon.explainers.test_explainer import TestExplainer
@@ -23,28 +26,54 @@ model_funs = [
     # simplemlp_model
 ]
 
+explainers = [
+    TestExplainer()
+]
+
 def main(args: Namespace):
-    explainer = TestExplainer()
     model: BaseModel
-    for model in model_funs:
-        
+
+    for model in (pbar0 := tqdm(model_funs)):
         model_name = model.__module__.split('.')[-1]
         log_dir = f'./hackathon/logs/{model_name}/expl'
+        if os.path.isdir(log_dir):
+            shutil.rmtree(log_dir)
         checkpoint_path = f'./hackathon/logs/{model_name}/xval/final/final.ckpt'
 
         runner = ModelRunner(log_dir=log_dir, quickrun=args.quickrun, seed=910)
-        #model.load_from_checkpoint(checkpoint_path)
-        model = torch.load(checkpoint_path)
-        test_dataloader = runner.data_setup(fold=-1).test_dataloader()
 
-        explanations = explainer.get_explanations(model, test_dataloader)
+        model = torch.load(checkpoint_path)
+        val_dataloader = runner.data_setup(fold=-1).val_dataloader()
+
+        pbar0.set_description(f'Model     {"<"+model_name+">":>30}')
+
+        for explainer in (pbar1 := tqdm(explainers, leave=False)):
+            explainer_name = type(explainer).__name__.lower()
+            pbar1.set_description(f'Explainer {"<"+explainer_name+">":>30}')
+
+            log_dir_explainer = os.path.join(log_dir, explainer_name)
+            if os.path.isdir(log_dir_explainer):
+                shutil.rmtree(log_dir_explainer)
+            else:
+                os.makedirs(log_dir_explainer)
+
+            explanations = explainer.get_explanations(
+                model=model,
+                val_dataloader=val_dataloader,
+                gpu=None if args.gpu == -1 else args.gpu
+            )
+
+            explanations.to_netcdf(os.path.join(log_dir_explainer, 'explanations.nc'))
+
+            val_dataloader.dataset.reset_sensitivity_ds()
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument(
         '--quickrun',
         action='store_true',
-        help='Quick development run with only 1 epoch and 2 CV folds, less data.')
+        help='Quick development run, explanations are computed for only one location .')
     parser.add_argument(
         '--gpu',
         type=int,
